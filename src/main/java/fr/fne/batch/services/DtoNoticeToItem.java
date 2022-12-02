46bc0d5df41ebcda25d02278cabd9abcf20fa9fc
charot <charot@abes.fr>
2022-12-02 16:44:30 +0100

    Maj DTO

--- src/main/java/fr/fne/batch/services/DtoNoticeToItem.java
+++ src/main/java/fr/fne/batch/services/DtoNoticeToItem.java
@@ -30,63 +30,125 @@ public class DtoNoticeToItem {
     /**
      * DTO :
      * Notice MarcXML vers ItemDocument WB
-     * @param record
+     * @param r
      * @param props
      * @return
      */
-    public ItemDocument unmarshallerNotice(Record record, Map<String,String> props)
+    public ItemDocument unmarshallerNotice(Record r, Map<String,String> props)
     {
         ItemDocumentBuilder itemDocumentBuilder = ItemDocumentBuilder.forItemId(ItemIdValue.NULL);
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
 
+            //200$a, 200$b, 200$f [001]
             String label = "";
+            //001
             String ppn = "";
-            String description = "";
-            for (Controlfield controlField : record.getControlfieldList()){
-                if (controlField.getTag().equalsIgnoreCase("001")){
-                    ppn = controlField.getValue();
+
+            //ControlFields :
+            for (Controlfield c : r.getControlfieldList()){
+                if (c.getTag().equalsIgnoreCase("001")){
+                    ppn = c.getValue(); //Pour construire le label
                 }
+
+                if (c.getTag().equalsIgnoreCase("003")){
+                    itemDocumentBuilder = itemDocumentBuilder.withDescription(c.getValue(), "fr");
+                    itemDocumentBuilder = this.addStmt(itemDocumentBuilder,props,"URL pérenne", c.getValue());
+                }
+
+                //Ajout de statements pour tous les controlFields :
+                itemDocumentBuilder = this.addStmt(itemDocumentBuilder,props,"Zone"+c.getTag(),c.getValue());
             }
 
-            for (Datafield dataField : record.getDatafieldList()){
-                if (dataField.getTag().equalsIgnoreCase("200")){
-                    for (Subfield subfield : dataField.getSubfieldList()){
-                        if (subfield.getCode().equalsIgnoreCase("a")){
-                            label += subfield.getValue();
-                        }
-                        else if (subfield.getCode().equalsIgnoreCase("b")){
-                            label += ", " + subfield.getValue();
-                        }
-                        else if (subfield.getCode().equalsIgnoreCase("f")){
-                            label += ", " + subfield.getValue();
+            //DataFields :
+            for (Datafield d : r.getDatafieldList()){
+
+                if (d.getTag().equalsIgnoreCase("010")) {
+                    for (Subfield s : d.getSubfieldList()) {
+                        if (s.getCode().equalsIgnoreCase("a")) {
+                            itemDocumentBuilder = this.addStmt(itemDocumentBuilder,props,"Identifiant ISNI", s.getValue());
                         }
                     }
-                    description = label; //objectMapper.writeValueAsString(dataField);
                 }
+
+                if (d.getTag().equalsIgnoreCase("101")) {
+                    for (Subfield s : d.getSubfieldList()) {
+                        if (s.getCode().equalsIgnoreCase("a")) {
+                            itemDocumentBuilder = this.addStmt(itemDocumentBuilder,props,"Lanque de l'oeuvre", s.getValue());
+                        }
+                    }
+                }
+
+                if (d.getTag().equalsIgnoreCase("103")) {
+                    for (Subfield s : d.getSubfieldList()) {
+                        if (s.getCode().equalsIgnoreCase("a")) {
+                            itemDocumentBuilder = this.addStmt(itemDocumentBuilder,props,"Date de naissance", s.getValue());
+                        }
+                        else if (s.getCode().equalsIgnoreCase("b")) {
+                            itemDocumentBuilder = this.addStmt(itemDocumentBuilder,props,"Date de décès", s.getValue());
+                        }
+                    }
+                }
+
+                if (d.getTag().equalsIgnoreCase("200")){
+                    for (Subfield s : d.getSubfieldList()){
+                        if (s.getCode().equalsIgnoreCase("a")){
+                            label += s.getValue();
+                            itemDocumentBuilder = this.addStmt(itemDocumentBuilder,props,"Nom", s.getValue());
+                        }
+                        else if (s.getCode().equalsIgnoreCase("b")){
+                            label += ", " + s.getValue();
+                            itemDocumentBuilder = this.addStmt(itemDocumentBuilder,props,"Prénom", s.getValue());
+                        }
+                        else if (s.getCode().equalsIgnoreCase("f")){
+                            label += ", " + s.getValue();
+                        }
+                    }
+                }
+
+                if (d.getTag().equalsIgnoreCase("240")){
+                    for (Subfield s : d.getSubfieldList()){
+                        if (s.getCode().equalsIgnoreCase("t")){
+                            itemDocumentBuilder = this.addStmt(itemDocumentBuilder,props,"Titre de l'oeuvre", s.getValue());
+                        }
+                    }
+                }
+
+                //Ajout de statements pour tous les dataFields :
+                itemDocumentBuilder = this.addStmt(itemDocumentBuilder,props,"Zone"+d.getTag(),objectMapper.writeValueAsString(d));
             }
 
-            Statement statement1 = StatementBuilder
-                    .forSubjectAndProperty(ItemIdValue.NULL, new PropertyIdValueImpl(props.get("Identifiant de la zone"),iriWikiBase))
-                    .withValue(Datamodel.makeStringValue(description)).build();
+            //Ajout du label construit : 200$a, 200$b, 200$f [001]
+            itemDocumentBuilder = itemDocumentBuilder.withLabel(label+" ["+ppn+"]", "fr");
 
-            Statement statement2 = StatementBuilder
-                    .forSubjectAndProperty(ItemIdValue.NULL, new PropertyIdValueImpl(props.get("Nom"),iriWikiBase))
-                    .withValue(Datamodel.makeStringValue(label)).build();
-
-            itemDocumentBuilder = itemDocumentBuilder
-                                    .withLabel(label+" ["+ppn+"]", "fr")
-                                    .withDescription(description, "fr");
-
-            itemDocumentBuilder = itemDocumentBuilder.withStatement(statement1);
-            itemDocumentBuilder = itemDocumentBuilder.withStatement(statement2);
         }
         catch (Exception e) {
-            logger.error("Erreur sur la notice : " + record + " :" + e.getMessage());
+            logger.error("Erreur sur la notice : " + r + " :" + e.getMessage());
             e.printStackTrace();
         }
         return itemDocumentBuilder.build();
+    }
+
+
+    private ItemDocumentBuilder addStmt(ItemDocumentBuilder itemDocumentBuilder, Map<String,String> props, String propriete, String valeur){
+        //Manque les références : "references": [{
+        //                                                "P2": ["Marc_XXX"],
+        //                                                "P1": ["<copie intégrale de la zone XXX>"]
+        //                                        }]
+        //Si la propriété ZoneXXX est connue :
+
+        if (props.get(propriete)!=null) {
+            Statement statement = StatementBuilder
+                    .forSubjectAndProperty(ItemIdValue.NULL, new PropertyIdValueImpl(props.get(propriete), iriWikiBase))
+                    .withValue(Datamodel.makeStringValue(valeur))
+                    .build();
+            itemDocumentBuilder = itemDocumentBuilder.withStatement(statement);
+        }
+        else {
+            logger.error("Il faut ajouter la propriété : "+propriete+" au fichier ProprietesWB.txt");
+        }
+        return itemDocumentBuilder;
     }
 
 }

923c86c30d6833eaf92f526720878b620d5ff437
charot <charot@abes.fr>
2022-12-09 14:44:21 +0100

    Ajout de la commande pour mettre à jour le SPARQL (après chargement par SQL)

--- src/main/java/fr/fne/batch/services/ChargementParSQL.java
+++ src/main/java/fr/fne/batch/services/ChargementParSQL.java
@@ -132,6 +132,10 @@ public class ChargementParSQL {
              * php extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipLinks –indexOnSkip
              * php extensions/CirrusSearch/maintenance/ForceSearchIndex.php –skipParse
              * php maintenance/runJobs.php
+             *
+             * Pour indexer dans WDQS (SPARQL) :
+             * Depuis le conteneur WDQS :
+             * /wdqs/runUpdate.sh -h http://${WDQS_HOST}:${WDQS_PORT} -- --wikibaseUrl ${WIKIBASE_SCHEME}://${WIKIBASE_HOST} --conceptUri ${WIKIBASE_SCHEME}://${WIKIBASE_HOST} --entityNamespaces ${WDQS_ENTITY_NAMESPACES} --init --start 20210315120000
             */
 
         } catch (Exception e) {
